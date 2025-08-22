86c96f1dc4459b380b6328853ca9017e9d494c27
Wai-Shing Luk <luk036@gmail.com>
2025-08-22 20:25:24 +0800

    Add more unit tests

--- test/source/test_ilds.cpp
+++ test/source/test_ilds.cpp
@@ -1,9 +1,14 @@
 #include <doctest/doctest.h>  // for ResultBuilder, TestCase, CHECK
 
 #include <ldsgen/ilds.hpp>  // for Halton
+#include <vector>           // for vector
 
-TEST_CASE("vdc_i") {
-    CHECK_EQ(ildsgen::vdc_i(1, 2, 10), 512);
+TEST_CASE("vdc_i") { CHECK_EQ(ildsgen::vdc_i(1, 2, 10), 512); }
+
+TEST_CASE("vdc_i with different values") {
+    CHECK_EQ(ildsgen::vdc_i(0, 2, 10), 0);
+    CHECK_EQ(ildsgen::vdc_i(2, 2, 10), 256);
+    CHECK_EQ(ildsgen::vdc_i(3, 2, 10), 768);
 }
 
 TEST_CASE("VdCorput_i") {
@@ -11,6 +16,31 @@ TEST_CASE("VdCorput_i") {
     CHECK_EQ(vgen.pop(), 512);
     vgen.reseed(0);
     CHECK_EQ(vgen.pop(), 512);
+}
+
+TEST_CASE("VdCorput_i multiple pops") {
+    auto vgen = ildsgen::VdCorput(2, 10);
+    vgen.reseed(0);
+    CHECK_EQ(vgen.pop(), 512);
+    CHECK_EQ(vgen.pop(), 256);
+    CHECK_EQ(vgen.pop(), 768);
+    CHECK_EQ(vgen.pop(), 128);
+}
+
+TEST_CASE("VdCorput_i reseed with different values") {
+    auto vgen = ildsgen::VdCorput(2, 10);
+    vgen.reseed(1);
+    CHECK_EQ(vgen.pop(), 256);
+    vgen.reseed(2);
+    CHECK_EQ(vgen.pop(), 768);
+    vgen.reseed(0);
+    CHECK_EQ(vgen.pop(), 512);
+}
+
+TEST_CASE("VdCorput_i different base and scale") {
+    auto vgen = ildsgen::VdCorput(3, 8);
+    vgen.reseed(0);
+    CHECK_EQ(vgen.pop(), 2187);  // 3^7 = 2187
 }
 
 TEST_CASE("Halton_i") {
@@ -24,4 +54,27 @@ TEST_CASE("Halton_i") {
     const auto arr2 = hgen.pop();
     CHECK_EQ(arr2[0], 1024);
     CHECK_EQ(arr2[1], 729);
+}
+
+TEST_CASE("Halton_i different bases and scales") {
+    const size_t base[] = {3, 5};
+    const unsigned int scale[] = {6, 4};
+    auto hgen = ildsgen::Halton(base, scale);
+    hgen.reseed(0);
+
+    auto res = hgen.pop();
+    CHECK_EQ(res[0], 243);  // 3^5 = 243
+    CHECK_EQ(res[1], 125);  // 5^3 = 125
+}
+
+TEST_CASE("Halton_i three dimensions") {
+    const size_t base[] = {2, 3, 5};
+    const unsigned int scale[] = {10, 8, 6};
+    auto hgen = ildsgen::Halton(base, scale);
+    hgen.reseed(0);
+
+    auto res = hgen.pop();
+    CHECK_EQ(res[0], 512);   // 2^9 = 512
+    CHECK_EQ(res[1], 2187);  // 3^7 = 2187
+    CHECK_EQ(res[2], 3125);  // 5^5 = 3125
 }
--- test/source/test_lds.cpp
+++ test/source/test_lds.cpp
@@ -2,9 +2,7 @@
 
 #include <ldsgen/lds.hpp>  // for Circle, Halton, Sphere, Sphere3Hopf
 
-TEST_CASE("vdc") {
-    CHECK_EQ(ldsgen::vdc(11, 2), doctest::Approx(0.8125));
-}
+TEST_CASE("vdc") { CHECK_EQ(ldsgen::vdc(11, 2), doctest::Approx(0.8125)); }
 
 TEST_CASE("VdCorput") {
     auto vgen = ldsgen::VdCorput(2);
@@ -12,6 +10,14 @@ TEST_CASE("VdCorput") {
     CHECK_EQ(vgen.pop(), doctest::Approx(0.25));
     CHECK_EQ(vgen.pop(), doctest::Approx(0.75));
     CHECK_EQ(vgen.pop(), doctest::Approx(0.125));
+    vgen.reseed(0);
+    CHECK_EQ(vgen.pop(), doctest::Approx(0.5));
+}
+
+TEST_CASE("VdCorput::reseed with non-zero") {
+    auto vgen = ldsgen::VdCorput(2);
+    vgen.reseed(5);
+    CHECK_EQ(vgen.pop(), doctest::Approx(0.375));
     vgen.reseed(0);
     CHECK_EQ(vgen.pop(), doctest::Approx(0.5));
 }
@@ -27,6 +33,29 @@ TEST_CASE("Circle") {
     CHECK_EQ(arr2[1], doctest::Approx(0.0));
 }
 
+TEST_CASE("Circle::pop multiple") {
+    auto cgen = ldsgen::Circle(2);
+    cgen.reseed(0);
+    auto res = cgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-1.0));
+    CHECK_EQ(res[1], doctest::Approx(0.0));
+    res = cgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.0));
+    CHECK_EQ(res[1], doctest::Approx(1.0));
+}
+
+TEST_CASE("Circle::reseed with non-zero") {
+    auto cgen = ldsgen::Circle(2);
+    cgen.reseed(2);
+    auto res = cgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.0));
+    CHECK_EQ(res[1], doctest::Approx(-1.0));
+    cgen.reseed(0);
+    res = cgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-1.0));
+    CHECK_EQ(res[1], doctest::Approx(0.0));
+}
+
 TEST_CASE("Disk") {
     auto dgen = ldsgen::Disk(2, 3);
     const auto arr = dgen.pop();
@@ -34,6 +63,29 @@ TEST_CASE("Disk") {
     dgen.reseed(0);
     const auto arr2 = dgen.pop();
     CHECK_EQ(arr2[0], doctest::Approx(-0.5773502692));
+}
+
+TEST_CASE("Disk::pop multiple") {
+    auto dgen = ldsgen::Disk(2, 3);
+    dgen.reseed(0);
+    auto res = dgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.5773502691896257));
+    CHECK_EQ(res[1], doctest::Approx(0.0));
+    res = dgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.0));
+    CHECK_EQ(res[1], doctest::Approx(0.816496580927726));
+}
+
+TEST_CASE("Disk::reseed with non-zero") {
+    auto dgen = ldsgen::Disk(2, 3);
+    dgen.reseed(2);
+    auto res = dgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.0));
+    CHECK_EQ(res[1], doctest::Approx(-0.3333333333333333));
+    dgen.reseed(0);
+    res = dgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.5773502691896257));
+    CHECK_EQ(res[1], doctest::Approx(0.0));
 }
 
 TEST_CASE("Halton") {
@@ -45,6 +97,29 @@ TEST_CASE("Halton") {
     CHECK_EQ(arr2[0], doctest::Approx(0.5));
 }
 
+TEST_CASE("Halton::pop multiple") {
+    auto hgen = ldsgen::Halton(2, 3);
+    hgen.reseed(0);
+    auto res = hgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.5));
+    CHECK_EQ(res[1], doctest::Approx(1.0 / 3.0));
+    res = hgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.25));
+    CHECK_EQ(res[1], doctest::Approx(2.0 / 3.0));
+}
+
+TEST_CASE("Halton::reseed with non-zero") {
+    auto hgen = ldsgen::Halton(2, 3);
+    hgen.reseed(5);
+    auto res = hgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.375));
+    CHECK_EQ(res[1], doctest::Approx(2.0 / 9.0));
+    hgen.reseed(0);
+    res = hgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(0.5));
+    CHECK_EQ(res[1], doctest::Approx(1.0 / 3.0));
+}
+
 TEST_CASE("Sphere") {
     auto sgen = ldsgen::Sphere(2, 3);
     const auto arr = sgen.pop();
@@ -54,6 +129,33 @@ TEST_CASE("Sphere") {
     CHECK_EQ(arr2[0], doctest::Approx(-0.5));
 }
 
+TEST_CASE("Sphere::pop multiple") {
+    auto sgen = ldsgen::Sphere(2, 3);
+    sgen.reseed(0);
+    auto res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.5));
+    CHECK_EQ(res[1], doctest::Approx(0.8660254037844387));
+    CHECK_EQ(res[2], doctest::Approx(0.0));
+    res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.4330127018922197));
+    CHECK_EQ(res[1], doctest::Approx(-0.75));
+    CHECK_EQ(res[2], doctest::Approx(-0.5));
+}
+
+TEST_CASE("Sphere::reseed with non-zero") {
+    auto sgen = ldsgen::Sphere(2, 3);
+    sgen.reseed(1);
+    auto res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.4330127018922197));
+    CHECK_EQ(res[1], doctest::Approx(-0.75));
+    CHECK_EQ(res[2], doctest::Approx(-0.5));
+    sgen.reseed(0);
+    res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.5));
+    CHECK_EQ(res[1], doctest::Approx(0.8660254037844387));
+    CHECK_EQ(res[2], doctest::Approx(0.0));
+}
+
 TEST_CASE("Sphere3Hopf") {
     auto shfgen = ldsgen::Sphere3Hopf(2, 3, 5);
     const auto arr = shfgen.pop();
@@ -61,6 +163,32 @@ TEST_CASE("Sphere3Hopf") {
     shfgen.reseed(0);
     const auto arr2 = shfgen.pop();
     CHECK_EQ(arr2[0], doctest::Approx(-0.22360679774997898));
+}
+
+TEST_CASE("Sphere3Hopf::pop multiple") {
+    auto sgen = ldsgen::Sphere3Hopf(2, 3, 5);
+    sgen.reseed(0);
+    auto res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.22360679774997898));
+    CHECK_EQ(res[1], doctest::Approx(0.3872983346207417));
+    CHECK_EQ(res[2], doctest::Approx(0.4472135954999573));
+    CHECK_EQ(res[3], doctest::Approx(-0.7745966692414837));
+}
+
+TEST_CASE("Sphere3Hopf::reseed with non-zero") {
+    auto sgen = ldsgen::Sphere3Hopf(2, 3, 5);
+    sgen.reseed(1);
+    auto res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.3162277660168382));
+    CHECK_EQ(res[1], doctest::Approx(-0.547722557505166));
+    CHECK_EQ(res[2], doctest::Approx(0.6708203932499367));
+    CHECK_EQ(res[3], doctest::Approx(-0.38729833462074204));
+    sgen.reseed(0);
+    res = sgen.pop();
+    CHECK_EQ(res[0], doctest::Approx(-0.22360679774997898));
+    CHECK_EQ(res[1], doctest::Approx(0.3872983346207417));
+    CHECK_EQ(res[2], doctest::Approx(0.4472135954999573));
+    CHECK_EQ(res[3], doctest::Approx(-0.7745966692414837));
 }
 
 TEST_CASE("dummy") { CHECK_EQ(ldsgen::dummy(15), 53); }
