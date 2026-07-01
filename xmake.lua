66fdecd9e8313e86df78ea332d206981288dca6c
Wai-Shing Luk <luk036@gmail.com>
2026-07-01 13:58:14 +0800

    fix: guard rapidcheck link with os.isfile() to fix macOS build

--- xmake.lua
+++ xmake.lua
@@ -96,14 +96,19 @@ target("test_wrapper_only")
     add_includedirs("include", {public = true})
     add_packages("spdlog", "fmt")
 
-    -- Check if rapidcheck was downloaded by CMake
+    -- Check if rapidcheck was built by CMake
     local rapidcheck_dir = path.join(os.projectdir(), "build", "_deps", "rapidcheck-src")
     local rapidcheck_lib_dir = path.join(os.projectdir(), "build", "_deps", "rapidcheck-build")
+    local rapidcheck_lib = nil
+
     if is_plat("windows") then
         rapidcheck_lib_dir = path.join(rapidcheck_lib_dir, "Release")
+        rapidcheck_lib = path.join(rapidcheck_lib_dir, "rapidcheck.lib")
+    else
+        rapidcheck_lib = path.join(rapidcheck_lib_dir, "librapidcheck.a")
     end
 
-    if os.isdir(rapidcheck_dir) and os.isdir(rapidcheck_lib_dir) then
+    if os.isdir(rapidcheck_dir) and os.isfile(rapidcheck_lib) then
         add_includedirs(path.join(rapidcheck_dir, "include"))
         add_linkdirs(rapidcheck_lib_dir)
         add_links("rapidcheck")
